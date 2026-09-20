d39322423bacbea0bbb35e1e0131d611dc3b736b
Nick McDowall <nicholasinengland@hotmail.com>
2026-09-20 11:18:46 +0100

    chore: upgrade to Spring Boot 4.1.1 (#40)
    Align with sibling connectors (firestore Boot 4 wave). Companion bumps:
    Spring Cloud 2025.1.3, GCP BOM 7.4.10, Kotlin 2.4.20, Gradle 9.7.1,
    git-version 4.3.0, LSD connector/logging, Testcontainers 2.x coords.
    
    TestRestTemplate moved to spring-boot-resttestclient (+ restclient).
    Supersedes closed Dependabot #37/#38 with a full companion upgrade.

--- src/test/kotlin/io/lsdconsulting/lsd/distributed/pubsub/integration/testapp/config/TestRestTemplateConfig.kt
+++ src/test/kotlin/io/lsdconsulting/lsd/distributed/pubsub/integration/testapp/config/TestRestTemplateConfig.kt
@@ -1,7 +1,7 @@
 package io.lsdconsulting.lsd.distributed.pubsub.integration.testapp.config
 
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.resttestclient.TestRestTemplate
 import org.springframework.context.annotation.Bean
 
 @TestConfiguration
